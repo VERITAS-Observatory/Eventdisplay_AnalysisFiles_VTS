c8364d788bbbe648e251a160742375d52be68f1a
GernotMaier <gernot.maier@desy.de>
2020-06-05 10:26:37 +0200

    Minor script updates to have a consistent cut lsit

--- EffectiveAreas/print_missing_effectiveArea_files.sh
+++ EffectiveAreas/print_missing_effectiveArea_files.sh
@@ -10,7 +10,8 @@ do
    do
       for T in T1234 T123 T124 T134 T234
       do
-         for C in NTel2-PointSource-Hard-TMVA-BDT NTel2-PointSource-Moderate NTel2-PointSource-Moderate-TMVA-BDT NTel2-PointSource-Moderate-TMVA-Preselection NTel2-PointSource-Soft NTel2-PointSource-Soft-TMVA-BDT NTel2-PointSource-Soft-TMVA-Preselection NTel3-PointSource-Hard NTel3-PointSource-Hard-TMVA-BDT
+         # for C in NTel2-PointSource-Hard-TMVA-BDT NTel2-PointSource-Moderate NTel2-PointSource-Moderate-TMVA-BDT NTel2-PointSource-Moderate-TMVA-Preselection NTel2-PointSource-Soft NTel2-PointSource-Soft-TMVA-BDT NTel2-PointSource-Soft-TMVA-Preselection NTel3-PointSource-Hard NTel3-PointSource-Hard-TMVA-BDT
+         for C in NTel2-PointSource-Hard-TMVA-BDT NTel2-PointSource-Moderate-TMVA-BDT NTel2-PointSource-Soft-TMVA-BDT NTel3-PointSource-Hard-TMVA-BDT
          do
              EFF="${VE}-${C}-${ME}-${E}-${A}-${T}.root"
              if [[ ! -e ${EFF} ]]; then
