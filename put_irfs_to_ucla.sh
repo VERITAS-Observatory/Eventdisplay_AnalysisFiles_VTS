c9cf65084e19f329a0c8468560102e0ef33048d7
GernotMaier <gernot.maier@desy.de>
2020-08-11 13:36:18 +0200

    add redHV putting/getting scripts

--- put_irfs_to_ucla.sh
+++ put_irfs_to_ucla.sh
@@ -27,20 +27,31 @@ do
 done
 
 # Effective areas for different epochs
-for I in V6_2012_2013a V6_2012_2013b V6_2013_2014a V6_2013_2014b V6_2014_2015 V6_2015_2016 V6_2016_2017 V6_2017_2018 V6_2018_2019 V6_2019_2020
+for F in nominalHV RedHV
 do
-   for A in ATM61 ATM62
-   do
-       for T in T1234 T123 T124 T134 T234
+    for I in V6_2012_2013a V6_2012_2013b V6_2013_2014a V6_2013_2014b V6_2014_2015 V6_2015_2016 V6_2016_2017 V6_2017_2018 V6_2018_2019 V6_2019_2020
+    do
+       for A in ATM61 ATM62
        do
-           D="EffectiveAreas_${I}_${A}_${T}"
-           echo "Uploading EffectiveAreas $I $A ${T} to /veritas/upload/EVNDISP/${VERSION}/${D}.tar "
-           rm -f -v ${D}.tar
-           tar -cvf ${D}.tar EffectiveAreas/*${I}*${A}*${T}.root
+           for T in T1234 T123 T124 T134 T234
+           do
+               D="EffectiveAreas_${I}_${A}_${T}"
+               if [[ ${F} == "RedHV" ]]; then
+                   D="EffectiveAreas_${F}_${I}_${A}_${T}"
+               fi
+               echo "Uploading EffectiveAreas $F $I $A ${T} to /veritas/upload/EVNDISP/${VERSION}/${D}.tar "
+               rm -f -v ${D}.tar
+               if [[ ${F} == "RedHV" ]]; then
+                   tar -cvf ${D}.tar EffectiveAreas/*${F}*${I}*${A}*${T}.root
+              # (lazy inconsistency: nominal HV files contain also redHV files)
+               else
+                   tar -cvf ${D}.tar EffectiveAreas/*${I}*${A}*${T}.root
+               fi
 
-           bbftp -u bbftp -V -S -m -p 12 -e "put ${D}.tar /veritas/upload/EVNDISP/${VERSION}/${D}.tar" gamma1.astro.ucla.edu
-       done
-    done
+               bbftp -u bbftp -V -S -m -p 12 -e "put ${D}.tar /veritas/upload/EVNDISP/${VERSION}/${D}.tar" gamma1.astro.ucla.edu
+           done
+        done
+     done
 done
 
 for I in V4 V5
