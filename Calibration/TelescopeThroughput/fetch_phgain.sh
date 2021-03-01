ea4d44e0efdb6b01dfcee4b4563389cb62c1eba8
Mireia Nievas <mireia.nievas-rosillo[AT]desy[dot]com>
2021-03-01 19:41:30 +0100

    Update Throughput directory

--- Calibration/TelescopeThroughput/fetch_phgain.sh
+++ Calibration/TelescopeThroughput/fetch_phgain.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
 
+# Photostat gains live here:
 # https://www.hep.physics.mcgill.ca/~veritas/photostat/
 
+rm -rf processed_gain_all_runs_no_CFD.csv
 wget https://www.hep.physics.mcgill.ca/~veritas/photostat/processed_gain_all_runs_no_CFD.csv
