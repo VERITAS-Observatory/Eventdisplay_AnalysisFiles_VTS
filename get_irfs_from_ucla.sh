dc9db624c06f5be011937f17827da5aa8132612a
GernotMaier <gernot.maier@desy.de>
2021-12-21 13:47:53 +0100

    update ucla directory

--- get_irfs_from_ucla.sh
+++ get_irfs_from_ucla.sh
@@ -13,7 +13,7 @@ command -v bbftp >/dev/null 2>&1 || { echo >&2 "bbftp is not installed. Aborting
 # Eventdisplay version
 VERSION=$(cat IRFVERSION)
 if [[ $IRFTYPE ]]; then
-   VERSION=${VERSION}-${IRFTYPE}
+   VERSION=${VERSION}${IRFTYPE}
 fi
 
 echo "Downloading IRF packages for ${VERSION}/"
