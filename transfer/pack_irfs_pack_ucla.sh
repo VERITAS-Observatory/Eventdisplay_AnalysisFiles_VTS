ddda4d669f296de0c1741e313a5040a17c7288a3
GernotMaier <gernot.maier@desy.de>
2023-07-16 12:26:14 +0200

    improved error handling of tar packing

--- transfer/pack_irfs_pack_ucla.sh
+++ transfer/pack_irfs_pack_ucla.sh
@@ -6,28 +6,41 @@
 #
 
 # directory with all packages
-DDIR="tar_packages"
-mkdir -p ${DDIR}
+DDIR="tar_packages_v490.2"
 
 # list of cuts
-CLISTNV=$(cat IRF_GAMMAHADRONCUTS.dat)
-CLISTRV=$(cat IRF_GAMMAHADRONCUTS_RedHV.dat)
-CLISTUV=$(cat IRF_GAMMAHADRONCUTS_UV.dat)
+CLISTNV=$(cat ../IRF_GAMMAHADRONCUTS.dat)
+CLISTNN=$(cat ../IRF_GAMMAHADRONCUTS_NN.dat)
+CLISTRV=$(cat ../IRF_GAMMAHADRONCUTS_RedHV.dat)
+CLISTUV=$(cat ../IRF_GAMMAHADRONCUTS_UV.dat)
 
 # cleaning types
 CLEANING="AP NN"
 
-P=$(PWD)
+P=$(pwd)
 cd ..
+mkdir -p ${DDIR}
+
+get_epochs()
+{
+    EPOCHS=$(cat IRF_EPOCHS_* | sort -u)
+    if [[ $1 = "NN" ]]; then
+        TEPOCHS="${EPOCHS[@]}"
+    else
+        TEPOCHS="${EPOCHS[@]} V4 V5"
+    fi
+    echo "$TEPOCHS"
+}
+
 
 pack_lookup_tables()
 {
     echo "Packing Lookup Tables"
     echo "====================="
-    EPOCHS=$(cat IRF_EPOCHS_* | sort -u)
     for C in ${CLEANING}
     do
-        for I in ${EPOCHS[@]} V4 V5
+        EPOCHS=$(get_epochs $C)
+        for I in ${EPOCHS[@]}
         do
             D="Tables_${C}_${I}"
             echo "Packing tables ${I} into ${D}.tar"
@@ -42,9 +55,9 @@ pack_gammahadronbdts()
 {
     echo "Packing GammaHadron BDTs"
     echo "========================"
-    EPOCHS=$(cat IRF_EPOCHS_* | sort -u)
     for C in ${CLEANING}
     do
+        EPOCHS=$(cat IRF_EPOCHS_* | sort -u)
         for I in ${EPOCHS[@]} V4 V5
         do
             D="GammaHadronBDTs_${C}_${I}"
@@ -60,9 +73,9 @@ pack_dispbdts()
 {
     echo "Packing disp BDTs"
     echo "========================"
-    EPOCHS=$(cat IRF_EPOCHS_* | sort -u)
     for C in ${CLEANING}
     do
+        EPOCHS=$(get_epochs $C)
         for I in ${EPOCHS[@]} V4 V5
         do
             D="DispBDTs_${C}_${I}"
@@ -106,7 +119,9 @@ pack_effectiveareas_V6()
                       ASAVE="ATM61"
                    fi
                    # list of cuts depend on observation mode
-                   if [[ ${F} == "RedHV" ]]; then
+                   if [[ ${C} == "NN" ]]; then
+                      CLIST=${CLISTNN}
+                   elif [[ ${F} == "RedHV" ]]; then
                       CLIST=${CLISTRV}
                    elif [[ ${F} == "UV" ]]; then
                       CLIST=${CLISTUV}
@@ -120,12 +135,13 @@ pack_effectiveareas_V6()
                        else
                            NFIL=$(find EffectiveAreas -name "*${T}*${C}*${I}*${A}*.root" | wc -l)
                        fi
+                       echo "FFFFFFF $NFIL"
                        if [[ $NFIL != "0" ]]; then
                            D="EffectiveAreas_${F}_${C}_${I}_${A}_${T}"
                            echo "Packing EffectiveAreas $F $C $I $A ${T} ${D}.tar ($NFIL files)"
                            rm -f -v ${D}.tar
                            if [[ ${F} == "RedHV" ]] || [[ ${F} == "UV" ]]; then
-                               tar -cvf ${D}.tar EffectiveAreas/*${F}*${C}*${T}*${I}*${A}*.root
+                               tar -cvf ${D}.tar EffectiveAreas/*${F}*${T}*${C}*${I}*${A}*.root
                            else
                                tar -cvf ${D}.tar EffectiveAreas/*${T}*${C}*${I}*${A}*.root
                            fi
@@ -147,6 +163,9 @@ pack_effectivareas_V4V5()
     echo "============================="
     for C in ${CLEANING}
     do
+        if [[ $C = "NN" ]]; then
+            continue
+        fi
         for I in V4 V5
         do
             for A in ATM21 ATM22
@@ -169,14 +188,14 @@ pack_effectivareas_V4V5()
     done
 }
 
-# pack_lookup_tables
+pack_lookup_tables
 
-# pack_effectiveareas_V6
+pack_effectiveareas_V6
 
-# pack_effectivareas_V4V5
+pack_effectivareas_V4V5
 
 pack_dispbdts
  
-# pack_gammahadronbdts
+pack_gammahadronbdts
 
 cd ${P}
