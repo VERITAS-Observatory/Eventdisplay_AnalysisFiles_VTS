ea6133b5d54229dfce21e56fb6950cdab31650f9
GernotMaier <gernot.maier@desy.de>
2023-12-14 13:36:43 +0100

    new version

--- transfer/put_irfs_to_ucla.sh
+++ transfer/put_irfs_to_ucla.sh
@@ -2,7 +2,7 @@
 # upload IRFs to UCLA
 # - uploading in tar packages
 # - one tar package per directory
-# 
+#
 # prepare by hand the upload directory:
 # bbftp -u bbftp -V -S -m -p 12 -e "mkdir /veritas/upload/EVNDISP/v490" gamma1.astro.ucla.edu
 #
@@ -14,7 +14,7 @@ command -v bbftp >/dev/null 2>&1 || { echo >&2 "bbftp is not installed. Aborting
 
 # Eventdisplay version
 VERSION=$(cat ../IRFVERSION)
-VERSION="${VERSION}.2"
+VERSION="${VERSION}.5"
 if [[ $IRFTYPE ]]; then
    VERSION=${VERSION}${IRFTYPE}
 fi
