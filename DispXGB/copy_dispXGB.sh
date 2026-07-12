65a27fb260c874ae7ed905028ef574042104f1c2
GernotMaier <gernot.maier@desy.de>
2026-07-12 12:43:30 +0200

    copy script

--- DispXGB/copy_dispXGB.sh
+++ DispXGB/copy_dispXGB.sh
@@ -2,49 +2,58 @@
 # copy dispXGB model files from IRF production site
 # Stereo and gamma/hadron analysis
 #
-# hardwired
-# - SIMTYPE (e.g., CARE_June2020)
-#
+
+if [ "$#" -lt 1 ]; then
+echo "
+./copy_dispXGB.sh <simulation type> [epochs (default: all)] [atmosphere (default \"61 62\")]
+
+   Copy dispXGB model files (stereo reconstruction, classification)
+
+   simulation types: CARE_202404 CARE_RedHV_Feb2024
+
+"
+exit
+fi
 
 IRFVERSION=$(cat ../IRFVERSION)
 ANALYSISTYPE="${VERITAS_ANALYSIS_TYPE:0:2}"
-# Set SIMTYPE to the desired simulation type (uncomment one):
-# SIMTYPE="CARE_UV_2212"
-# SIMTYPE="GRISU"
-# SIMTYPE="CARE_RedHV"
-# SIMTYPE="CARE_June2020"
-# SIMTYPE="CARE_24_20"
-# SIMTYPE="CARE_RedHV_Feb2024"
-SIMTYPE="CARE_202404"
+SIMTYPE="${1}"
+EPOCHS=${2:-"all"}
+ATM=${3:-"61 62"}
 
 echo "COPY dispXGB  for ${IRFVERSION}, analysis type ${ANALYSISTYPE}, and simulation type ${SIMTYPE}"
+echo "  Atmosphere $ATM"
+echo "  EPOCHS $EPOCHS"
 
 STEREO_PAR="$VERITAS_EVNDISP_AUX_DIR/ParameterFiles/XGB-stereo-parameter.json"
 IDS=$(jq -r '.zenith[].id' $STEREO_PAR)
 echo "Zenith bins: $IDS"
 
-for A in ATM61 ATM62
+for A in $ATM
 do
+    if [[ $EPOCHS == "all" ]]; then
+        if [[ ${SIMTYPE} == "GRISU" ]]; then
+            EPOCHS="V4 V5"
+        elif [[ ${SIMTYPE} == *"UV"* ]]; then
+            EPOCHS=$(cat ../IRF_EPOCHS_obsfilter.dat | sort -u)
+            if [[ ${A} == "ATM62" ]]; then
+                continue
+            fi
+        else
+            if [[ ${A} == "ATM62" ]]; then
+                EPOCHS=$(cat ../IRF_EPOCHS_SUMMER.dat | sort -u)
+            else
+                EPOCHS=$(cat ../IRF_EPOCHS_WINTER.dat | sort -u)
+            fi
+        fi
+    fi
+    A="ATM${A}"
     if [[ ${SIMTYPE} == "GRISU" ]]; then
         A=${A/6/2}
-        EPOCHS="V4 V5"
-    elif [[ ${SIMTYPE} == *"UV"* ]]; then
-        EPOCHS=$(cat ../IRF_EPOCHS_obsfilter.dat | sort -u)
-        if [[ ${A} == "ATM62" ]]; then
-            continue
-        fi
-    else
-        if [[ ${A} == "ATM62" ]]; then
-            EPOCHS=$(cat ../IRF_EPOCHS_SUMMER.dat | sort -u)
-        else
-            EPOCHS=$(cat ../IRF_EPOCHS_WINTER.dat | sort -u)
-        fi
-        # TODO fixed epoch
-        # EPOCHS="V6_2016_2017"
     fi
     for E in $EPOCHS
     do
-        echo "EPOCH ${E} ATMO ${A} (**NOTE EPOCHS ARE IGNORED**)"
+        echo "EPOCH ${E} ATMO ${A}"
         if [[ ${SIMTYPE} == *"RedHV"* ]]; then
             ODIR="${VERITAS_ANALYSIS_TYPE:0:2}/${E}_${A}_redHV"
         elif [[ ${SIMTYPE} == *"UV"* ]]; then
@@ -61,13 +70,13 @@ do
             echo "Zenith bin $Z"
             ZDIR="${ODIR}/${Z}"
             mkdir -p ${ZDIR}
-            SDIR="${IDIR}/${E}_${A}_gamma/TrainXGBStereoAnalysisBinned/${Z}"
+            SDIR="${IDIR}/${E}_${A}_gamma/TrainXGBStereoAnalysis/${Z}"
             cp -v -f ${SDIR}/* ${ZDIR}/
         done
 
         ##################################################
         # Gamma/hadron BDTs (zenith angle independent)
         GDIR="${IDIR}/${E}_${A}_gamma/TrainXGBGammaHadron"
-        cp -v -i ${GDIR}/*.{joblib,log} ${ODIR}/
+        cp -v -f ${GDIR}/*.{joblib,log} ${ODIR}/
     done
 done
